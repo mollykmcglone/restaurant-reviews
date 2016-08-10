6956987d641eb7761b3a7039162440c69df3d4c1
Molly Mcglone and Noah Yasskin <molly@>
2016-08-10 09:56:27 -0700

    add views to app components and review model

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from 'angular2/core';
+import { ReviewListComponent } from './review-list.component';
+import { Review } from './review.model';
 
 @Component({
   selector: 'my-app',
+  directives: [ReviewListComponent],
   template: `
     <div class="container">
-      <h1>New Skeleton Angular2 App!</h1>
+      <h1>*ngEat!</h1>
+      <h3 *ngFor="#review of reviews">{{ review.name}}, Specialty: {{review.specialty}}, {{review.address}}, Price: {{review.price}}.</h3>
     </div>
   `
 })
 
 export class AppComponent {
-
+  public reviews: Review[];
+  constructor(){
+    this.reviews = [
+      new Review("Cibo", "Italian", "3539 SE Division Street, Portland, OR 97202", "moderate"),
+      new Review("Mi Mero Mole", "Mexican", "32 NW 5th Avenue, Portland, OR, 97209", "cheap"),
+      new Review("Nicholas", "Lebanese", "318 SE Grand Avenue, Portland, OR, 97214", "cheap"),
+      new Review("Farm Spirit", "American", "1414 SE Morrison Street, Portland, OR, 97214", "expensive")
+    ];
+  }
 }
---
+++ app/review.model.ts
@@ -0,0 +1,6 @@
+export class Review {
+  constructor(public name: string, public specialty: string, public address: string, public price: string, public id: number)
+  {
+    
+  }
+}
