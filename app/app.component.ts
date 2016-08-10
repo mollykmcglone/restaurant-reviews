6882a8149c58dfd6fe83de5d442abb4dc9bf8bdb
Molly Mcglone and Noah Yasskin <noah@>
2016-08-10 10:04:07 -0700

    add event emitter to list items

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from 'angular2/core';
-import { ReviewListComponent } from './review-list.component';
 import { Review } from './review.model';
 
 @Component({
   selector: 'my-app',
-  directives: [ReviewListComponent],
   template: `
     <div class="container">
       <h1>*ngEat!</h1>
-      <h3 *ngFor="#review of reviews">{{ review.name}}, Specialty: {{review.specialty}}, {{review.address}}, Price: {{review.price}}.</h3>
+      <h3 *ngFor="#review of reviews" (click)="reviewWasSelected(review)">{{ review.name}}, Specialty: {{review.specialty}}, {{review.address}}, Price: {{review.price}}.</h3>
     </div>
   `
 })
@@ -17,10 +15,13 @@ export class AppComponent {
   public reviews: Review[];
   constructor(){
     this.reviews = [
-      new Review("Cibo", "Italian", "3539 SE Division Street, Portland, OR 97202", "moderate"),
-      new Review("Mi Mero Mole", "Mexican", "32 NW 5th Avenue, Portland, OR, 97209", "cheap"),
-      new Review("Nicholas", "Lebanese", "318 SE Grand Avenue, Portland, OR, 97214", "cheap"),
-      new Review("Farm Spirit", "American", "1414 SE Morrison Street, Portland, OR, 97214", "expensive")
+      new Review("Cibo", "Italian", "3539 SE Division Street, Portland, OR 97202", "moderate", 0),
+      new Review("Mi Mero Mole", "Mexican", "32 NW 5th Avenue, Portland, OR, 97209", "cheap", 1),
+      new Review("Nicholas", "Lebanese", "318 SE Grand Avenue, Portland, OR, 97214", "cheap", 2),
+      new Review("Farm Spirit", "American", "1414 SE Morrison Street, Portland, OR, 97214", "expensive", 3)
     ];
+  }
+  reviewWasSelected(clickedReview: Review): void {
+    console.log(clickedReview);
   }
 }
